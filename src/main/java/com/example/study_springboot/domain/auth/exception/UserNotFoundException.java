93c255d99c91651572b8d977620b8e1aaeb87b19
Woonseok Yang <yws3786@gmail.com>
2022-09-17 22:05:43 +0900

    [Feat] UserNotFoundException 추가

---
+++ src/main/java/com/example/study_springboot/domain/auth/exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.example.study_springboot.domain.auth.exception;
+
+import com.example.study_springboot.global.error.CustomException;
+import com.example.study_springboot.global.error.ErrorCode;
+
+public class UserNotFoundException extends CustomException {
+
+    public static final UserNotFoundException EXCEPTION =
+            new UserNotFoundException();
+
+    private UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+
+}
