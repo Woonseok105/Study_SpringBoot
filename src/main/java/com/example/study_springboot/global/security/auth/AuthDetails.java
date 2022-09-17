8895ad884d233b6004a6568aca6812601f50ae3d
Woonseok Yang <yws3786@gmail.com>
2022-09-17 23:31:20 +0900

    [Feat] userDetails 추가

---
+++ src/main/java/com/example/study_springboot/global/security/auth/AuthDetails.java
@@ -0,0 +1,59 @@
+package com.example.study_springboot.global.security.auth;
+
+import com.example.study_springboot.domain.auth.domain.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    //사용자에게 부여된 권한을 반환합니다.
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+    //사용자를 인증하는 데 사용된 암호를 반환합니다.
+
+    @Override
+    public String getUsername() {
+        return user.getAccountId();
+    }
+    //사용자를 인증하는 데 사용된 사용자 이름을 반환합니다.
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    //사용자의 계정이 만료되었는지 여부를 나타냅니다.
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    //사용자가 잠겨 있는지 또는 잠금 해제되어 있는지 나타냅니다.
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    //사용자의 자격 증명(암호)이 만료되었는지 여부를 나타냅니다.
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    //사용자가 활성화되었는지 비활성화되었는지 여부를 나타냅니다.
+
+}
---
+++ src/main/java/com/example/study_springboot/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.study_springboot.global.security.auth;
+
+import com.example.study_springboot.domain.auth.domain.repository.UserRepository;
+import com.example.study_springboot.domain.auth.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
+        return userRepository.findByAccountId(accountId)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
